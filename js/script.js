b1a5510c933a0acfd08b423ac83429875237d19f
imRajAryan09 <raj.12020162@lpu.in>
2023-01-28 21:40:41 +0530

    hero done

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let tl = gsap.timeline();
+// tl.set({ delay: 0 });
+
+tl.to('#header', {
+	duration: 1,
+	transform: 'translateY(0%)',
+})
+	.to('#hero', {
+		duration: 1,
+		width: 'max-content',
+		opacity: 1,
+	})
+	.to(['#main-heading', '#sub-heading', '#cta-button'], {
+		duration: 1,
+		right: '0%',
+		opacity: 1,
+		stagger: 0.5,
+	})
+	.to('#hero-img', {
+		duration: 1,
+		opacity: 1,
+		left: 0,
+	});
