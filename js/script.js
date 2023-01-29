b0eeb93f8376c8914b9512524bf3c02fe65ce9c5
imRajAryan09 <raj.12020162@lpu.in>
2023-01-29 19:14:36 +0530

    anim done

--- js/script.js
+++ js/script.js
@@ -1,24 +1,57 @@
+let li = document.querySelectorAll('body header nav ul li a');
+li.forEach(item => {
+	item.addEventListener('click', e => {
+		e.preventDefault();
+		if (item.textContent === 'About') {
+			document.querySelector('body').style.background = '#82cbf8';
+		} else if (item.textContent === 'Contact') {
+			document.querySelector('body').style.background = '#f8c982';
+		} else {
+			document.querySelector('body').style.background = '#ffdca8';
+		}
+	});
+});
+
 let tl = gsap.timeline();
 
-if (window.innerWidth > 768) {
-	tl.to('#header', {
+if (window.innerWidth > 425) {
+	tl.from('#logo', {
 		duration: 1,
-		transform: 'translateY(0%)',
+		yPercent: -100,
 	})
-		.to('#hero', {
+		.from(
+			'#li-text',
+			{
+				duration: 1,
+				stagger: 0.1,
+				ease: 'power0.none',
+				width: 0,
+			},
+			'<'
+		)
+		.fromTo(
+			'#hero',
+			{
+				duration: 1.5,
+				opacity: 0,
+			},
+			{
+				opacity: 1,
+			}
+		)
+		.from(['#main-heading', '#sub-heading', '#cta-button'], {
+			right: '150%',
 			duration: 1,
-			width: 'max-content',
-			opacity: 1,
-		})
-		.to(['#main-heading', '#sub-heading', '#cta-button'], {
-			duration: 1,
-			right: '0%',
-			opacity: 1,
 			stagger: 0.5,
+			ease: 'power4.out',
 		})
-		.to('#hero-img', {
-			duration: 1,
-			opacity: 1,
-			left: 0,
-		});
+		.from(
+			'#hero-img',
+			{
+				duration: 1,
+				opacity: 0,
+				left: '100%',
+			},
+			'<'
+		);
 }
