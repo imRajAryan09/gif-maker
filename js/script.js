caac3728727186d8bbe2b79509b401e7ad1a049e
imRajAryan09 <raj.12020162@lpu.in>
2023-01-29 19:18:36 +0530

    anim done

--- js/script.js
+++ js/script.js
@@ -13,45 +13,42 @@ li.forEach(item => {
 });
 
 let tl = gsap.timeline();
-
-if (window.innerWidth > 425) {
-	tl.from('#logo', {
-		duration: 1,
-		yPercent: -100,
-	})
-		.from(
-			'#li-text',
-			{
-				duration: 1,
-				stagger: 0.1,
-				ease: 'power0.none',
-				width: 0,
-			},
-			'<'
-		)
-		.fromTo(
-			'#hero',
-			{
-				duration: 1.5,
-				opacity: 0,
-			},
-			{
-				opacity: 1,
-			}
-		)
-		.from(['#main-heading', '#sub-heading', '#cta-button'], {
-			right: '150%',
+tl.from('#logo', {
+	duration: 1,
+	yPercent: -100,
+})
+	.from(
+		'#li-text',
+		{
 			duration: 1,
-			stagger: 0.5,
-			ease: 'power4.out',
-		})
-		.from(
-			'#hero-img',
-			{
-				duration: 1,
-				opacity: 0,
-				left: '100%',
-			},
-			'<'
-		);
-}
+			stagger: 0.1,
+			ease: 'power0.none',
+			width: 0,
+		},
+		'<'
+	)
+	.fromTo(
+		'#hero',
+		{
+			duration: 1.5,
+			opacity: 0,
+		},
+		{
+			opacity: 1,
+		}
+	)
+	.from(['#main-heading', '#sub-heading', '#cta-button'], {
+		right: '150%',
+		duration: 1,
+		stagger: 0.5,
+		ease: 'power4.out',
+	})
+	.from(
+		'#hero-img',
+		{
+			duration: 1,
+			opacity: 0,
+			left: '100%',
+		},
+		'<'
+	);
